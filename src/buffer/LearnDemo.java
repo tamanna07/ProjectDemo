7b35c0b3727a309f5ca68b07c03d0094a22219b9
Tamanna Sultana <tsbristy@gmail.com>
2019-09-05 11:43:33 -0500

    just checking

--- src/buffer/LearnDemo.java
+++ src/buffer/LearnDemo.java
@@ -6,6 +6,8 @@ public class LearnDemo {
     public static void main(String[] args) {
 
         System.out.println("HEllo");
+        System.out.println("HEllo rifath");
+        System.out.println("HEllo raihan");
     }
 
 }
